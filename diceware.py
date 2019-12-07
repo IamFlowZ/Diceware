4095b19f42f48715017a0810c5515e6aefee94b2
iamflowz <dakota.lewallen17@gmail.com>
2019-12-07 16:14:47 -0500

    reduced the main func into little functs

--- diceware.py
+++ diceware.py
@@ -1,10 +1,16 @@
 """
     diceware main module
 """
+
 import os
-import re
 import sys
+import argparse
+import re
 import random
+
+my_parser = argparse.ArgumentParser(prog='diceware', description='Create a passphrase based on the diceware standard.', epilog='Stay safe!')
+my_parser.add_argument('-n', '--num', type=int, help='Number of words to be concatanated.')
+my_parser.add_argument('-r', '--replace', type=str, help='Comma delimited list of characters to replace in output. Formatted like, \"original:replacement\"(e.g. a:@,A:^)')
 
 def retrieve_word_list():
     """ parse wordlist file into dictionary """
@@ -19,68 +25,75 @@ def retrieve_word_list():
                 split_list[key_value[0]] = key_value[1].replace('\n', '')
     return split_list
 
-if __name__ == '__main__':
-    num_words = 0
-    key_len = 5
+def generate_keys(num_words):
     keys = []
-    split_list = retrieve_word_list()
+    for _ in range(int(num_words)):
+        key = ''
+        for _ in range(5):
+            key += (str(random.randint(1,6)))
+        keys  = keys + [key]
+
+    pwd = ''
+    for key in keys:
+        val = split_list[key]
+        up = val[0].upper()
+        temp = val[1:(len(val))]
+        m_temp = up + temp
+        pwd += m_temp
+
+    return pwd
+
+def get_num_words():
     print("With passwords, length > complexity.\nSo how many words would you like?\n")
+    num_words = input()
+    if num_words.isdecimal():
+        return num_words
+    else:
+        print("Please enter how many words you would like in your password.")
+        get_num_words()
 
-    run = True
-    while(run):
-        num_words = input()
-        if num_words.isdecimal():
-            for i in range(int(num_words)):
-                key = ''
-                for j in range(key_len):
-                    key += (str(random.randint(1,6)))
+def replace_char(input, old, new):
+    return input.replace(old, new)
 
-                keys  = keys + [key]
+def make_changes(pswd):
+    print("Please enter the character you wish to change, a \":\" followed by the new character.")
+    changes = input()
+    if len(changes) > 1 and ':' in changes:
+        new_vals = changes.split(':')
+        pswd = replace_char(pswd, new_vals[0], new_vals[1])
+        return pswd
+    else:
+        make_changes(pswd)
 
-            pwd = ''
-            for item in keys:
-                val = split_list[item]
-                up = val[0].upper()
-                temp = val[1:(len(val))]
-                m_temp = up + temp
-                pwd += m_temp
+def realtime_replace(pswd):
+    print("Would you like to replace any of the other characters? (y/n)")
+    answer = input()
+    answer.strip()
+    if answer.lower() == 'n' or answer.lower() == 'no':
+        print("Thank you for using Diceware, goodbye =^)")
+        return False
+    elif answer.lower() == 'y' or answer.lower() == 'yes':
+        pswd = make_changes(pswd)
+        print(f'Your current password:\t\033[92m{pswd}\033[0m')
+        realtime_replace(pswd)
+    else:
+        print("Please enter either y(yes) or n(no).")
+        realtime_replace(pswd)
 
-            print(pwd)
-            run = False
-        else:
-            print("Please enter how many words you would like in your password.")
+if __name__ == '__main__':
+    args = my_parser.parse_args()
+    split_list = retrieve_word_list()
+    if args.num is not None:
+        pswd = generate_keys(args.num)
+    else:
+        pswd = generate_keys(get_num_words())
 
-    run2 = True
-    while run2:
-        print("Would you like to replace any letters with special characters? (y/n)")
-        answer = input()
-        answer.strip()
-        
-        if answer.lower() == 'n':
-            print("Thank you for using Diceware, goodbye =^)")
-            run2 = False
-            sys.exit()
-            
-        elif answer.lower() == 'y':
-            print("Please enter how many characters you would like to replace,")
-            num_changes = input()
-            if num_changes.isdecimal():
-                for n in range(int(num_changes)):
-                    print("Please enter the character you wish to change, followed by new character.")
-                    changes = input()
-                    if len(changes) > 1:
-                        changes.split(" ")
-                        temp = list(pwd)
-                        for i in range(len(pwd)):
-                            if temp[i] == changes[0]:
-                                temp[i] = changes[2]
-                        pwd = "".join(temp)
-                        print("Here's your new password, " + pwd)
-                    else:
-                        print("Please enter the character you wish to replace followed by the new character")
-                
-            else:
-                print("Please enter the number of characters you would like to replace")
-                    
-        elif answer.lower() != 'y' or 'n':
-            print("Please enter either yes or no.")
+    if args.replace is not None:
+        replacements = args.replace.split(',')
+        for replacement in replacements:
+            new_vals = replacement.split(':')
+            pswd = replace_char(pswd, new_vals[0], new_vals[1])
+        print(f'Your current password:\t{pswd}')
+        realtime_replace(pswd)
+    else:
+        realtime_replace(pswd)
